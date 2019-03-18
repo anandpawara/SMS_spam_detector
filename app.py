385b74db0c7e4c1a135f6af209f400565d1cd601
Anand Pawara <anandpawara19@gmail.com>
2019-03-18 23:17:05 +0530

    added libraries

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return '<h1>Hello World</h1>'
 
+
 if __name__ == '__main__':
     app.run()
-
-
-
-
